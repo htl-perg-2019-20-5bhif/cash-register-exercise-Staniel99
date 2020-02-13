d9695b4abee7e57ea6fe3d24cc96fbadc51cee06
Staniel99 <stefan.daniel99@gmx.at>
2020-02-13 06:56:29 +0100

    Code added

---
+++ CashRegister/CashRegister.UICore/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashRegister.UICore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set the data context for data binding
+            //DataContext = ViewModel = new MainWindowViewModel();
+        }
+    }
+
+    public class MainWindowViewModel : BindableBase
+    {
+        /*private ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products
+        {
+            get { return products; }
+            set { SetProperty(ref products, value); }
+        }*/
+    }
+}
